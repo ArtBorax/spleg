0498a053bbfdc419eba3e95de3cdb8ecdb345fdf
ArtBorax <ArtBorax@gmail.com>
2018-04-26 22:49:06 +0300

    реализован мейн класс, подправлен конфиг градла.

---
+++ src/main/java/net/bor/spleg/Test.java
@@ -0,0 +1,14 @@
+package net.bor.spleg;
+
+// @author ArtBorax
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(i);
+        }
+        System.exit(0);
+        System.out.println("end");
+    }
+}
