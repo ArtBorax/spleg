be6cbfbf804e4aa20cae36644f99ef70ed0a1730
ArtBorax <ArtBorax@gmail.com>
2018-04-26 14:45:56 +0300

    !!!Bamp!!! new project

---
+++ src/main/java/net/bor/spleg/Spleg.java
@@ -0,0 +1,22 @@
+package net.bor.spleg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Spleg extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("disabled.");
+    }
+
+}
